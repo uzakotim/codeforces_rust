6fafb869656c754d3f5c7d3eb6fd7df779a2c46f
uzakotim <timauzakov@gmail.com>
2025-09-15 18:31:52 +0500

    removing target

--- a_healthy_menu/target/debug/a_healthy_menu.d
+++
@@ -1,1 +0,0 @@
-/Users/timuruzakov/git/codeforces_rust/a_healthy_menu/target/debug/a_healthy_menu: /Users/timuruzakov/git/codeforces_rust/a_healthy_menu/src/main.rs
--- a_healthy_menu/target/debug/deps/a_healthy_menu-898e18696b564f3a.d
+++
@@ -1,9 +0,0 @@
-/Users/timuruzakov/git/codeforces_rust/a_healthy_menu/target/debug/deps/liba_healthy_menu-898e18696b564f3a.rmeta: src/main.rs Cargo.toml
-
-/Users/timuruzakov/git/codeforces_rust/a_healthy_menu/target/debug/deps/a_healthy_menu-898e18696b564f3a.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
